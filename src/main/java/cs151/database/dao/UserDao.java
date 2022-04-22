6b3656a0e63291974302c378f581179de2c46923
Nhat Le <nhat.n.le@sjsu.edu>
2022-04-21 22:21:18 -0700

    create User DAO (aks UserDB) and finish implementing 'search' method

---
+++ src/main/java/cs151/database/dao/UserDao.java
@@ -0,0 +1,51 @@
+package cs151.database.dao;
+
+import cs151.database.models.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+// User Data Access Object
+public class UserDao {
+    private Connection conn;
+    // inject Connection object
+    public UserDao(Connection conn) {
+        this.conn = conn;
+    }
+
+    //search if a user exist (for login)
+    public User search(String username) {
+        // SQL Query template
+        String query = "SELECT * FROM user WHERE userId = ?";
+        // Java Query Object
+        PreparedStatement ps = null;
+        User user = null;
+        try {
+            // initialize Java Query Object with the template
+            ps = this.conn.prepareStatement(query);
+            // fill the template
+            ps.setString(1, username);
+            // call query on the database, check if user exist
+            if(ps.execute())
+            {
+                //user found
+                //Get all the results
+                ResultSet users = ps.getResultSet();
+                //extract data from result
+                String userId = users.getString("userId");
+                String password = users.getString("password");
+                String securityQuestion = users.getString("securityQuestion");
+                String securityAnswer = users.getString("securityAnswer");
+                //create user object for returning
+                user = new User(userId, password, securityQuestion, securityAnswer);
+            }
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return user;
+    }
+
+}
